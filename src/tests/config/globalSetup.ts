35abb4fc888f5f2c7aea5c4b203e2e39b3d5f6fd
Martin Trajanovski <martin.trajanovski@sigmatechnology.se>
2020-02-28 10:13:00 +0100

    Do not expose user credentials in tests and use of variables with GraphQL

--- src/tests/config/globalSetup.ts
+++ src/tests/config/globalSetup.ts
@@ -1,6 +1,9 @@
 import { exec } from 'child_process';
 import * as util from 'util';
 
+import { config } from 'dotenv';
+config({ path: '.env.test' });
+
 export default async () => {
   const CMD_DROP_SEED_DATABASE = `${__dirname}/../scripts/runDropAndSeedTestDB.sh`;
   const pExec = util.promisify(exec);
--- src/tests/integration/todo.resolver.test.ts
+++ src/tests/integration/todo.resolver.test.ts
@@ -1,3 +1,5 @@
+import faker from 'faker';
+
 import { apiCall } from '@src/utils/testHelpers';
 
 describe('Test todos CRUD', () => {
@@ -8,21 +10,32 @@ describe('Test todos CRUD', () => {
         id
         title
         completed
+        createdBy {
+          id
+          firstName
+          roles
+        }
       }
     }
   `;
 
   beforeAll(async () => {
+    const { USER_USERNAME, USER_PASSWORD } = process.env;
+    const loginData = {
+      email: USER_USERNAME,
+      password: USER_PASSWORD,
+    };
+
     const loginQuery = `
-      query {
-        login(data: {email: "test@test.com", password: "test123"}) {
+      query login($loginData: LoginUserInput!) {
+        login(data: $loginData) {
           firstName
           token
         }
       }
     `;
 
-    const loginResponse = await apiCall(loginQuery);
+    const loginResponse = await apiCall(loginQuery, null, { loginData });
 
     token = loginResponse.data.login.token;
   });
@@ -36,10 +49,70 @@ describe('Test todos CRUD', () => {
     expect(todos).toMatchSnapshot();
   });
 
+  test('When we try to read the all todos with USER role, it should return `Wrong credentials` error', async () => {
+    const getAllTodosQuery = `
+    query {
+      getAllTodos {
+        id
+        title
+        completed
+      }
+    }
+  `;
+    const allTodosResponse = await apiCall(getAllTodosQuery, token);
+    const data = allTodosResponse.data;
+
+    expect(data).toBeNull();
+    expect(allTodosResponse.errors).toBeDefined();
+    expect(allTodosResponse.errors[0].message).toBe('Wrong credentials');
+  });
+
+  test('When we try to read the all todos with ADMIN role, it should match the snapshot', async () => {
+    const getAllTodosQuery = `
+      query {
+        getAllTodos {
+          id
+          title
+          completed,
+          createdBy {
+            id
+            firstName
+            roles
+          }
+        }
+      }
+    `;
+    const { ADMIN_USERNAME, ADMIN_PASSWORD } = process.env;
+    const loginData = {
+      email: ADMIN_USERNAME,
+      password: ADMIN_PASSWORD,
+    };
+
+    const loginQuery = `
+      query login($loginData: LoginUserInput!) {
+        login(data: $loginData) {
+          firstName
+          token
+        }
+      }
+    `;
+
+    const loginResponse = await apiCall(loginQuery, null, { loginData });
+    const adminToken = loginResponse.data.login.token;
+
+    const allTodosResponse = await apiCall(getAllTodosQuery, adminToken);
+    const { getAllTodos } = allTodosResponse.data;
+
+    expect(getAllTodos).toMatchSnapshot();
+  });
+
   test('When we try to create a todo, it should return that todo with id', async () => {
+    const newTodo = {
+      title: faker.random.words(),
+    };
     const createTodoMutation = `
-      mutation {
-        createTodo(data: {title: "New test todo"}) {
+      mutation createTodo($newTodo: CreateTodoInput!) {
+        createTodo(data: $newTodo) {
           id
           title
           completed
@@ -47,22 +120,27 @@ describe('Test todos CRUD', () => {
       }
     `;
 
-    const todosResponse = await apiCall(createTodoMutation, token);
+    const todosResponse = await apiCall(createTodoMutation, token, { newTodo });
     const { createTodo } = todosResponse.data;
 
     expect(createTodo).toBeDefined();
     expect(createTodo.id).toBeDefined();
-    expect(createTodo.title).toBe('New test todo');
+    expect(createTodo.title).toBe(newTodo.title);
     expect(createTodo.completed).toBeFalsy();
   });
 
   test('When we try to update a todo, it should return that todo updated', async () => {
     const initialTodosResponse = await apiCall(query, token);
     const initialTodos = initialTodosResponse.data.todos;
-    const todoToUpdate = initialTodos[initialTodos.length - 1];
+    const todoToUpdate = {
+      id: initialTodos[initialTodos.length - 1].id,
+      title: faker.random.words(),
+      completed: true,
+    };
+
     const updateTodoMutation = `
-      mutation {
-        updateTodo(data: {id: "${todoToUpdate.id}", title: "Test todo updated", completed: true}) {
+      mutation updateTodo($todoToUpdate: UpdateTodoInput!) {
+        updateTodo(data: $todoToUpdate) {
           id
           title
           completed
@@ -70,11 +148,13 @@ describe('Test todos CRUD', () => {
       }
     `;
 
-    const removeTodoResponse = await apiCall(updateTodoMutation, token);
+    const removeTodoResponse = await apiCall(updateTodoMutation, token, {
+      todoToUpdate,
+    });
     const { updateTodo } = removeTodoResponse.data;
 
     expect(updateTodo).toBeDefined();
-    expect(updateTodo.title).toBe('Test todo updated');
+    expect(updateTodo.title).toBe(todoToUpdate.title);
     expect(updateTodo.completed).toBeTruthy();
   });
 
@@ -83,12 +163,14 @@ describe('Test todos CRUD', () => {
     const initialTodos = initialTodosResponse.data.todos;
     const todoToRemove = initialTodos[initialTodos.length - 1];
     const removeTodoMutation = `
-      mutation {
-        removeTodo(id: "${todoToRemove.id}")
+      mutation removeTodo($id: String!) {
+        removeTodo(id: $id)
       }
     `;
 
-    const removeTodoResponse = await apiCall(removeTodoMutation, token);
+    const removeTodoResponse = await apiCall(removeTodoMutation, token, {
+      id: todoToRemove.id,
+    });
     const { removeTodo } = removeTodoResponse.data;
 
     expect(removeTodo).toBeTruthy();
